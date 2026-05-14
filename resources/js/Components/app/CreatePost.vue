6a04dd4c49981186cac3bc9089448d49bc39c3a0
Mina-Melad-1 <menamelad586@gmail.com>
2026-05-14 19:59:29 +0300

    Define website home page

---
+++ resources/js/Components/app/CreatePost.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+
+const creatingPost = ref(false);
+defineProps({});
+</script>
+
+<template>
+  <div class="p-4 bg-white rounded-lg border mb-4">
+    <div class="py-3 px-2 text-gray-500 border border-2 border-gray-300 rounded mb-3" @click="creatingPost = true">
+      Click here to create a new post
+    </div>
+    <div v-if="creatingPost" class="flex gap-2 justify-between">
+      <button
+        type="submit"
+        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 relative"
+      >
+        Attach Files
+        <input type="file" class="absolute left-0 top-0 right-0 bottom-0 opacity-0" />
+      </button>
+      <button
+        type="submit"
+        class="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+      >
+        Submit
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ resources/js/Components/app/PostList.vue
@@ -0,0 +1,68 @@
+<script setup>
+import PostItem from "./PostItem.vue";
+const post1 = {
+  user: {
+    id: 1,
+    avatar: "https://i.pravatar.cc/150?img=1",
+    name: "John Doe",
+  },
+  group: null,
+  attachment: [
+    {
+      id: 1,
+      name: "image1.jpg",
+      url: "https://picsum.photos/1000",
+      mime: "image/jpeg",
+    },
+    {
+      id: 2,
+      name: "image2.jpg",
+      url: "https://picsum.photos/1000",
+      mime: "image/jpeg",
+    },
+    {
+      id: 3,
+      name: "document.docs",
+      url: "https://example.com/document.pdf",
+      mime: "application/docs",
+    },
+  ],
+  body: `
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    `,
+  created_at: "2024-06-01 15:12",
+};
+
+const post2 = {
+  user: {
+    id: 2,
+    avatar: "https://i.pravatar.cc/150?img=2",
+    name: "Jane Smith",
+  },
+  group: {
+    id: 1,
+    name: "Vue.js Enthusiasts",
+  },
+  body: `
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+    `,
+  created_at: "2024-06-01 15:12",
+};
+</script>
+
+<template>
+  <div class="overflow-auto h-full">
+    <PostItem :post="post1" />
+    <PostItem :post="post2" />
+  </div>
+</template>
+
+<style scoped></style>
