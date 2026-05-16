524d6d7e6d413fc97a164390b6449469403a6210
Mina-Melad-1 <menamelad586@gmail.com>
2026-05-16 17:01:04 +0300

    Implement uploading cover image on user

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,35 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', [HomeController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
-Route::get('/u/{user:username}', [ProfileController::class, 'index'])->name('profile');
+Route::get('/', [HomeController::class, 'index'])
+    ->middleware(['auth', 'verified'])->name('dashboard');
 
-/* Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-}); */
+
+Route::get('/u/{user:username}', [ProfileController::class, 'index'])
+    ->name('profile');
+
+Route::middleware('auth')->group(function () {
+    Route::post('/profile/update-images', [ProfileController::class, 'updateImage'])
+        ->name('profile.updateCover');
+//    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
 require __DIR__.'/auth.php';
