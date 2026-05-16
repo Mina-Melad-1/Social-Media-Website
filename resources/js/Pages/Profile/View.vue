a25643c229a31205d19694dae4daa02606840a86
Mina-Melad-1 <menamelad586@gmail.com>
2026-05-16 20:20:48 +0300

    Implement uploading avatar image on user

--- resources/js/Pages/Profile/View.vue
+++ resources/js/Pages/Profile/View.vue
@@ -1,177 +1,257 @@
 <template>
-    <AuthenticatedLayout>
-        <div class="max-w-[768px] mx-auto h-full overflow-auto">
-            <div
-                v-show="showNotification && status === 'cover-image-update'"
-                class="my-2 py-2 px-3 font-medium text-sm bg-emerald-500 text-white"
+  <AuthenticatedLayout>
+    <div class="max-w-[768px] mx-auto h-full overflow-auto">
+      <div
+        v-show="showNotification && success"
+        class="my-2 py-2 px-3 font-medium text-sm bg-emerald-500 text-white"
+      >
+        {{ success }}
+      </div>
+      <div
+        v-if="errors.cover"
+        class="my-2 py-2 px-3 font-medium text-sm bg-red-400 text-white"
+      >
+        {{ errors.cover }}
+      </div>
+      <div class="group relative bg-white">
+        <img
+          :src="coverImageSrc || user.cover_url || '/img/default_cover.jpg'"
+          class="w-full h-[200px] object-cover"
+        />
+        <div class="absolute top-2 right-2">
+          <button
+            v-if="!coverImageSrc"
+            class="bg-gray-100 hover:bg-gray-200 text-gray-800 py-1 px-2 text-xs flex items-center opacity-0 group-hover:opacity-100"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="w-3 h-3 mr-2"
             >
-                Your cover image has been updated
-            </div>
-            <div
-                v-if="errors.cover"
-                class="my-2 py-2 px-3 font-medium text-sm bg-red-400 text-white"
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z"
+              />
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z"
+              />
+            </svg>
+
+            Update Cover Image
+            <input
+              type="file"
+              class="absolute left-0 top-0 bottom-0 right-0 opacity-0"
+              @change="onCoverChange"
+            />
+          </button>
+          <div v-else class="flex gap-2">
+            <button
+              @click="cancelCoverImage"
+              class="bg-red-700 hover:bg-red-800 text-white py-1 px-2 text-xs flex items-center"
             >
-                {{ errors.cover }}
-            </div>
-            <div class="group relative bg-white">
-                <img :src="coverImageSrc || user.cover_url || '/img/default_cover.jpg'"
-                     class="w-full h-[200px] object-cover">
-                <div class="absolute top-2 right-2 ">
-                    <button
-                        v-if="!coverImageSrc"
-                        class="bg-gray-50 hover:bg-gray-100 text-gray-800 py-1 px-2 text-xs flex items-center opacity-0 group-hover:opacity-100">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                             stroke="currentColor" class="w-3 h-3 mr-2">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                  d="M6.827 6.175A2.31 2.31 0 015.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 00-1.134-.175 2.31 2.31 0 01-1.64-1.055l-.822-1.316a2.192 2.192 0 00-1.736-1.039 48.774 48.774 0 00-5.232 0 2.192 2.192 0 00-1.736 1.039l-.821 1.316z"/>
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                  d="M16.5 12.75a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zM18.75 10.5h.008v.008h-.008V10.5z"/>
-                        </svg>
-
-                        Update Cover Image
-                        <input type="file" class="absolute left-0 top-0 bottom-0 right-0 opacity-0"
-                               @change="onCoverChange"/>
-                    </button>
-                    <div v-else class="flex gap-2">
-                        <button
-                            @click="cancelCoverImage"
-                            class="bg-gray-50 hover:bg-gray-100 text-gray-800 py-1 px-2 text-xs flex items-center">
-                            <XMarkIcon class="h-3 w-3 mr-2"/>
-                            Cancel
-                        </button>
-                        <button
-                            @click="submitCoverImage"
-                            class="bg-gray-800 hover:bg-gray-900 text-gray-100 py-1 px-2 text-xs flex items-center">
-                            <CheckCircleIcon class="h-3 w-3 mr-2"/>
-                            Submit
-                        </button>
-                    </div>
-                </div>
-
-                <div class="flex">
-                    <img src="https://cdn.iconscout.com/icon/free/png-256/free-avatar-370-456322.png?f=webp"
-                         class="ml-[48px] w-[128px] h-[128px] -mt-[64px]">
-                    <div class="flex justify-between items-center flex-1 p-4">
-                        <h2 class="font-bold text-lg">{{ user.name }}</h2>
-                        <PrimaryButton v-if="isMyProfile">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                 stroke="currentColor" class="w-4 h-4 mr-2">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"/>
-                            </svg>
-                            Edit Profile
-                        </PrimaryButton>
-                    </div>
-                </div>
-            </div>
-            <div class="border-t">
-                <TabGroup>
-                    <TabList class="flex bg-white">
-                        <Tab v-if="isMyProfile" v-slot="{ selected }" as="template">
-                            <TabItem text="About" :selected="selected"/>
-                        </Tab>
-                        <Tab v-slot="{ selected }" as="template">
-                            <TabItem text="Posts" :selected="selected"/>
-                        </Tab>
-                        <Tab v-slot="{ selected }" as="template">
-                            <TabItem text="Followers" :selected="selected"/>
-                        </Tab>
-                        <Tab v-slot="{ selected }" as="template">
-                            <TabItem text="Followings" :selected="selected"/>
-                        </Tab>
-                        <Tab v-slot="{ selected }" as="template">
-                            <TabItem text="Photos" :selected="selected"/>
-                        </Tab>
-                    </TabList>
-
-                    <TabPanels class="mt-2">
-                        <TabPanel v-if="isMyProfile">
-                            <Edit :must-verify-email="mustVerifyEmail" :status="status"/>
-                        </TabPanel>
-                        <TabPanel class="bg-white p-3 shadow">
-                            Posts
-                        </TabPanel>
-                        <TabPanel class="bg-white p-3 shadow">
-                            Followers
-                        </TabPanel>
-                        <TabPanel class="bg-white p-3 shadow">
-                            Followings
-                        </TabPanel>
-                        <TabPanel class="bg-white p-3 shadow">
-                            Photos
-                        </TabPanel>
-                    </TabPanels>
-                </TabGroup>
-            </div>
+              <XMarkIcon class="h-3 w-3 mr-2" />
+              Cancel
+            </button>
+            <button
+              @click="submitCoverImage"
+              class="bg-emerald-600 hover:bg-emerald-700 text-gray-100 py-1 px-2 text-xs flex items-center"
+            >
+              <CheckCircleIcon class="h-3 w-3 mr-2" />
+              Submit
+            </button>
+          </div>
         </div>
-    </AuthenticatedLayout>
+
+        <div class="flex">
+          <div
+            class="flex items-center justify-center relative group/avatar -mt-[64px] ml-[48px] w-[128px] h-[128px] rounded-full"
+          >
+            <img
+              :src="avatarImageSrc || user.avatar_url || '/img/default_avatar.jpg'"
+              class="w-full h-full object-cover rounded-full object-cover"
+            />
+            <button
+              v-if="!avatarImageSrc"
+              class="absolute left-0 top-0 bottom-0 right-0 bg-black/25 rounded-full flex items-center justify-center opacity-0 group-hover/avatar:opacity-100"
+            >
+              <CameraIcon class="h-7 w-7" />
+
+              <input
+                type="file"
+                class="absolute left-0 top-0 bottom-0 right-0 opacity-0"
+                @change="onAvatarChange"
+              />
+            </button>
+            <div v-else class="absolute top-1 right-0 flex flex-col gap-2">
+              <button
+                @click="cancelAvatarImage"
+                class="w-7 h-7 flex items-center justify-center bg-red-500/80 rounded-full text-white"
+              >
+                <XMarkIcon class="h-5 w-5" />
+              </button>
+              <button
+                @click="submitAvatarImage"
+                class="w-7 h-7 flex items-center justify-center bg-emerald-500/80 rounded-full text-white"
+              >
+                <CheckCircleIcon class="h-5 w-5" />
+              </button>
+            </div>
+          </div>
+          <div class="flex justify-between items-center flex-1 p-4">
+            <h2 class="font-bold text-lg">{{ user.name }}</h2>
+            <PrimaryButton v-if="isMyProfile">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-4 h-4 mr-2"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125"
+                />
+              </svg>
+              Edit Profile
+            </PrimaryButton>
+          </div>
+        </div>
+      </div>
+      <div class="border-t">
+        <TabGroup>
+          <TabList class="flex bg-white">
+            <Tab v-if="isMyProfile" v-slot="{ selected }" as="template">
+              <TabItem text="About" :selected="selected" />
+            </Tab>
+            <Tab v-slot="{ selected }" as="template">
+              <TabItem text="Posts" :selected="selected" />
+            </Tab>
+            <Tab v-slot="{ selected }" as="template">
+              <TabItem text="Followers" :selected="selected" />
+            </Tab>
+            <Tab v-slot="{ selected }" as="template">
+              <TabItem text="Followings" :selected="selected" />
+            </Tab>
+            <Tab v-slot="{ selected }" as="template">
+              <TabItem text="Photos" :selected="selected" />
+            </Tab>
+          </TabList>
+
+          <TabPanels class="mt-2">
+            <TabPanel v-if="isMyProfile">
+              <Edit :must-verify-email="mustVerifyEmail" :status="status" />
+            </TabPanel>
+            <TabPanel class="bg-white p-3 shadow"> Posts </TabPanel>
+            <TabPanel class="bg-white p-3 shadow"> Followers </TabPanel>
+            <TabPanel class="bg-white p-3 shadow"> Followings </TabPanel>
+            <TabPanel class="bg-white p-3 shadow"> Photos </TabPanel>
+          </TabPanels>
+        </TabGroup>
+      </div>
+    </div>
+  </AuthenticatedLayout>
 </template>
 
 <script setup>
-import {computed, ref} from 'vue'
-import {XMarkIcon, CheckCircleIcon} from '@heroicons/vue/24/solid'
-import {TabGroup, TabList, Tab, TabPanels, TabPanel} from '@headlessui/vue'
-import {usePage} from "@inertiajs/vue3";
+import { computed, ref } from "vue";
+import { XMarkIcon, CheckCircleIcon, CameraIcon } from "@heroicons/vue/24/solid";
+import { TabGroup, TabList, Tab, TabPanels, TabPanel } from "@headlessui/vue";
+import { usePage } from "@inertiajs/vue3";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import TabItem from "@/Pages/Profile/Partials/TabItem.vue";
 import Edit from "@/Pages/Profile/Edit.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
-import {useForm} from '@inertiajs/vue3'
+import { useForm } from "@inertiajs/vue3";
 
 const imagesForm = useForm({
-    avatar: null,
-    cover: null,
-})
+  avatar: null,
+  cover: null,
+});
 
-const showNotification = ref(true)
-const coverImageSrc = ref('')
+const showNotification = ref(true);
+const coverImageSrc = ref("");
+const avatarImageSrc = ref("");
 const authUser = usePage().props.auth.user;
 
-const isMyProfile = computed(() => authUser && authUser.id === props.user.id)
+const isMyProfile = computed(() => authUser && authUser.id === props.user.id);
 
 const props = defineProps({
-    errors: Object,
-    mustVerifyEmail: {
-        type: Boolean,
-    },
-    status: {
-        type: String,
-    },
-    user: {
-        type: Object
-    }
+  errors: Object,
+  mustVerifyEmail: {
+    type: Boolean,
+  },
+  status: {
+    type: String,
+  },
+  success: {
+    type: String,
+  },
+  user: {
+    type: Object,
+  },
 });
 
 function onCoverChange(event) {
-    imagesForm.cover = event.target.files[0]
-    if (imagesForm.cover) {
-        const reader = new FileReader()
-        reader.onload = () => {
-            coverImageSrc.value = reader.result;
-        }
-        reader.readAsDataURL(imagesForm.cover)
-    }
+  imagesForm.cover = event.target.files[0];
+  if (imagesForm.cover) {
+    const reader = new FileReader();
+    reader.onload = () => {
+      coverImageSrc.value = reader.result;
+    };
+    reader.readAsDataURL(imagesForm.cover);
+  }
+}
+
+function onAvatarChange(event) {
+  imagesForm.avatar = event.target.files[0];
+  if (imagesForm.avatar) {
+    const reader = new FileReader();
+    reader.onload = () => {
+      avatarImageSrc.value = reader.result;
+    };
+    reader.readAsDataURL(imagesForm.avatar);
+  }
 }
 
 function cancelCoverImage() {
-    imagesForm.cover = null;
-    coverImageSrc.value = null
+  imagesForm.cover = null;
+  coverImageSrc.value = null;
+}
+
+function cancelAvatarImage() {
+  imagesForm.avatar = null;
+  avatarImageSrc.value = null;
 }
 
 function submitCoverImage() {
-    imagesForm.post(route('profile.updateCover'), {
-        onSuccess: (user) => {
-            cancelCoverImage()
-            setTimeout(() => {
-                showNotification.value = false
-            }, 3000)
-        },
-    })
+  imagesForm.post(route("profile.updateImages"), {
+    onSuccess: (user) => {
+      cancelCoverImage();
+      setTimeout(() => {
+        showNotification.value = false;
+      }, 3000);
+    },
+  });
 }
 
+function submitAvatarImage() {
+  imagesForm.post(route("profile.updateImages"), {
+    onSuccess: (user) => {
+      cancelAvatarImage();
+      setTimeout(() => {
+        showNotification.value = false;
+      }, 3000);
+    },
+  });
+}
 </script>
 
-
-<style scoped>
-
-</style>
+<style scoped></style>
